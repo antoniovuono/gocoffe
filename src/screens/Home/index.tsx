d66d6d8a6f616e4076215800e86a4f92ded3ab51
antoniovuono <antoniosvuono@icloud.com>
2022-03-11 09:35:06 -0300

    Project initial start configuration with expo and typescript!

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Styled.Container>
+        <Styled.Title>Welcome ASV Studio</Styled.Title>
+    </Styled.Container>
+  );
+}
+
+export default Home;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+   flex: 1;
+   justify-content: center;
+   align-items: center;
+   background-color: black;
+`;
+
+export const Title = styled.Text`
+font-size: 30px;
+color: white;
+`;
