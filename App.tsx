68508f1c2d95b5ebe8363224f701e9ed63d86691
antoniovuono <antoniosvuono@icloud.com>
2022-03-12 10:08:58 -0300

    Theme configured and colors seted

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
 import Home from './src/screens/Home';
 
 export default function App() {
-    return <Home />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Home />
+        </ThemeProvider>
+    );
 }
---
+++ src/theme/index.tsx
@@ -0,0 +1,13 @@
+export default {
+    COLORS: {
+        primary_background: '#18171B',
+        secondary_background: '#26252B',
+        secondary: '#EA8C00',
+        primary_title: '#FFFFFF',
+        secondary_title: '#3F3F3F',
+        warning: '#FF0000',
+        success: '#34C800',
+    },
+
+    FONTS: {},
+};
