3c88ec21bb47561a888879f08672ed4c6aa3f324
antoniovuono <antoniosvuono@icloud.com>
2022-03-12 11:19:47 -0300

    Home screen started and header created

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,7 +4,11 @@ import * as Styled from './styles';
 const Home: React.FC = () => {
     return (
         <Styled.Container>
-            <Styled.Title>GOCOFFE</Styled.Title>
+            <Styled.Header>
+                <Styled.WelcomeTitle>Welcome Antonio,</Styled.WelcomeTitle>
+                <Styled.Title>Find the best coffe </Styled.Title>
+                <Styled.Title>for your day</Styled.Title>
+            </Styled.Header>
         </Styled.Container>
     );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,26 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
     background-color: ${({ theme }) => theme.COLORS.primary_background};
 `;
 
+export const Header = styled.View`
+    width: 100%;
+    margin-top: ${getStatusBarHeight() + 30}px;
+    padding: 0 20px;
+`;
+
+export const WelcomeTitle = styled.Text`
+    font-size: ${RFValue(16)}px;
+    color: ${({ theme }) => theme.COLORS.primary_title};
+    font-family: ${({ theme }) => theme.FONTS.regular};
+`;
+
 export const Title = styled.Text`
-    font-size: 30px;
-    font-family: ${({ theme }) => theme.FONTS.bold};
+    font-size: ${RFValue(24)}px;
+    font-family: ${({ theme }) => theme.FONTS.semi_bold};
     color: ${({ theme }) => theme.COLORS.primary_title};
 `;
