21f6931376a0dbcd2587a618846e09b0a0c14dd8
antoniovuono <antoniosvuono@icloud.com>
2022-03-14 12:58:30 -0300

    Title messages fix

--- src/screens/Messages/index.tsx
+++ src/screens/Messages/index.tsx
@@ -5,7 +5,7 @@ import * as Styled from './styles';
 const Messages: React.FC = () => {
     return (
         <Styled.Container>
-            <Styled.Title>Welcome ASV Studio (SHOPPING CART)</Styled.Title>
+            <Styled.Title>Welcome ASV Studio (MESSAGES CART)</Styled.Title>
         </Styled.Container>
     );
 };
