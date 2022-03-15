54c120bacf8e69eb84bb024eb7273c249476beb6
antoniovuono <antoniosvuono@icloud.com>
2022-03-14 22:13:56 -0300

    Product page started with header and product image created

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 import * as Styled from './styles';
 
 const Product: React.FC = () => {
+    const theme = useTheme();
+    const navigation = useNavigation();
+
+    const handleGoBack = () => {
+        navigation.goBack();
+    };
+
     return (
         <Styled.Container>
-            <Styled.Title>Welcome to PDP</Styled.Title>
+            <Styled.Header>
+                <Styled.ButtonGoBack
+                    hitSlop={{ left: 15, top: 15, right: 15, bottom: 15 }}
+                    onPress={handleGoBack}
+                >
+                    <Ionicons
+                        name="chevron-back"
+                        size={24}
+                        color={theme.COLORS.primary_title}
+                    />
+                </Styled.ButtonGoBack>
+
+                <Styled.HeaderTitle>Cappuccino</Styled.HeaderTitle>
+
+                <Styled.ButtonFavoriteProduct
+                    hitSlop={{ left: 15, top: 15, right: 15, bottom: 15 }}
+                >
+                    <MaterialIcons
+                        name="favorite-border"
+                        size={24}
+                        color={theme.COLORS.primary_title}
+                    />
+                </Styled.ButtonFavoriteProduct>
+            </Styled.Header>
+
+            <Styled.ProductImage
+                source={{
+                    uri: 'https://res.cloudinary.com/didxdzbfe/image/upload/v1647143105/gocoffe/Captura_de_Tela_2022-03-13_a%CC%80s_00.44.56_mioql3.png',
+                }}
+            />
         </Styled.Container>
     );
 };
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -1,10 +1,35 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.primary_background};
-    justify-content: center;
-    align-items: center;
+    width: 100%;
+    padding: 0 20px;
 `;
 
-export const Title = styled.Text``;
+export const Header = styled.View`
+    background-color: ${({ theme }) => theme.COLORS.primary_background};
+    width: 100%;
+    flex-direction: row;
+    margin-top: ${getStatusBarHeight() + 30}px;
+    justify-content: space-between;
+`;
+
+export const ButtonGoBack = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+    font-family: ${({ theme }) => theme.FONTS.medium};
+    font-size: ${RFValue(14)}px;
+    color: ${({ theme }) => theme.COLORS.primary_title};
+`;
+
+export const ButtonFavoriteProduct = styled.TouchableOpacity``;
+
+export const ProductImage = styled.Image`
+    width: 100%;
+    height: 40%;
+    border-radius: ${RFValue(20)}px;
+    margin-top: ${RFValue(20)}px;
+`;
