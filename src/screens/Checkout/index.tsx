dca62a5e067defb9ccb9b7d2380c168c0398d300
antoniovuono <antoniosvuono@icloud.com>
2022-03-26 14:34:46 -0300

    Confirm order screen finalized

--- src/screens/Checkout/index.tsx
+++ src/screens/Checkout/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { useNavigation } from '@react-navigation/native';
+import DoneIcon from '../../assets/icons/done.svg';
 import * as Styled from './styles';
 
 const Checkout: React.FC = () => {
-    return <Styled.Container />;
+    const navigation = useNavigation();
+
+    const handleConfirm = () => {
+        navigation.navigate('Home');
+    };
+
+    return (
+        <Styled.Container>
+            <DoneIcon style={{ marginBottom: 40 }} />
+            <Styled.CheckoutTitle>Order finalized!</Styled.CheckoutTitle>
+            <Styled.CheckoutDescription>
+                Go to the cashier to make your payment and pick up your order.
+            </Styled.CheckoutDescription>
+
+            <Styled.ButtonConfirm onPress={handleConfirm}>
+                <Styled.ButtonConfirmTitle>Ok</Styled.ButtonConfirmTitle>
+            </Styled.ButtonConfirm>
+        </Styled.Container>
+    );
 };
 
 export default Checkout;
--- src/screens/Checkout/styles.ts
+++ src/screens/Checkout/styles.ts
@@ -1,5 +1,36 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.COLORS.primary_background};
+`;
+
+export const CheckoutTitle = styled.Text`
+    font-size: ${RFValue(30)}px;
+    font-family: ${({ theme }) => theme.FONTS.semi_bold};
+    color: ${({ theme }) => theme.COLORS.primary_title};
+`;
+
+export const CheckoutDescription = styled.Text`
+    font-size: ${RFValue(15)}px;
+    font-family: ${({ theme }) => theme.FONTS.regular};
+    color: ${({ theme }) => theme.COLORS.primary_title};
+    text-align: center;
+    padding: 20px 60px;
+`;
+
+export const ButtonConfirm = styled.TouchableOpacity`
+    padding: 20px 40px;
+    background-color: ${({ theme }) => theme.COLORS.secondary_background};
+    border-radius: 5px;
+    margin-top: ${RFValue(90)}px;
+`;
+
+export const ButtonConfirmTitle = styled.Text`
+    font-size: ${RFValue(15)}px;
+    font-family: ${({ theme }) => theme.FONTS.regular};
+    color: ${({ theme }) => theme.COLORS.primary_title};
 `;
